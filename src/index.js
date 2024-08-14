47a2ecbb4c0c2e27ed12d390a4c9b596dc4f5270
omor777 <omor0843@gmail.com>
2024-08-14 22:56:03 +0600

    initial setup complete

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDb from "./db.js";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Ok" });
+});
+
+app.listen(port, () => {
+  console.log(`App listen on port ${port}`);
+});
+
+connectDb("mongodb://localhost:27017/ecoShopperDB")
+  .then(() => {
+    console.log("Connect to DB successful!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use((err, _req, res, _next) => {
+  console.log(err.stack);
+  res.status(500)({ error: "Something went wrong!" });
+});
