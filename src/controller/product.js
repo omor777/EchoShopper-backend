f128f4213f4f83d71f8cc396664483b2c7dfd8c9
omor777 <omor0843@gmail.com>
2024-08-15 21:05:34 +0600

    implement paginaiton functionality

--- src/controller/product.js
+++ src/controller/product.js
@@ -1,9 +1,24 @@
 import Product from "../models/product.model.js";
 
 const getAllProductsController = async (req, res, next) => {
+  const page = parseInt(req.query?.page) || 1;
+  const limit = parseInt(req.query?.limit) || 9;
+
   try {
-    const products = await Product.find();
-    res.status(200).json(products);
+    const products = await Product.find()
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    const productsCount = await Product.countDocuments();
+
+    const totalPages = Math.ceil(productsCount / limit);
+    const hasMore = page * limit < productsCount;
+
+    res.status(200).json({
+      products,
+      totalPages,
+      hasMore,
+    });
   } catch (e) {
     next(e);
   }
