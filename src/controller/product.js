894656cc2b3574a1d72b0220e43ce3b886ba9d29
omor777 <omor0843@gmail.com>
2024-08-16 15:32:57 +0600

    add sort functionality by price

--- src/controller/product.js
+++ src/controller/product.js
@@ -3,18 +3,28 @@ import Product from "../models/product.model.js";
 const getAllProductsController = async (req, res, next) => {
   const page = parseInt(req.query?.page) || 1;
   const limit = parseInt(req.query?.limit) || 9;
-  const search = req.query?.search || "";
+  const search = req.query?.search;
+  const sort = req.query?.sort;
 
-  const query = {
+  // filter related query
+  const filterQuery = {
     name: { $regex: search, $options: "i" },
   };
 
+  // sort related query
+  // TODO: sort by date newest product first appear
+  let sortQuery;
+  if (sort === "price-high-to-low" || sort === "price-low-to-high") {
+    sortQuery = { price: sort === "price-high-to-low" ? -1 : 1 };
+  }
+
   try {
-    const products = await Product.find(query)
+    const products = await Product.find(filterQuery)
+      .sort(sortQuery)
       .skip((page - 1) * limit)
       .limit(limit);
 
-    const productsCount = await Product.countDocuments(query);
+    const productsCount = await Product.countDocuments(filterQuery);
 
     const totalPages = Math.ceil(productsCount / limit);
     const hasMore = page * limit < productsCount;
