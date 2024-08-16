6561798eeb7f3c058bae41bb38dec1ccf0fdfda4
omor777 <omor0843@gmail.com>
2024-08-16 14:46:05 +0600

    add search functionality

--- src/controller/product.js
+++ src/controller/product.js
@@ -3,13 +3,18 @@ import Product from "../models/product.model.js";
 const getAllProductsController = async (req, res, next) => {
   const page = parseInt(req.query?.page) || 1;
   const limit = parseInt(req.query?.limit) || 9;
+  const search = req.query?.search || "";
+
+  const query = {
+    name: { $regex: search, $options: "i" },
+  };
 
   try {
-    const products = await Product.find()
+    const products = await Product.find(query)
       .skip((page - 1) * limit)
       .limit(limit);
 
-    const productsCount = await Product.countDocuments();
+    const productsCount = await Product.countDocuments(query);
 
     const totalPages = Math.ceil(productsCount / limit);
     const hasMore = page * limit < productsCount;
